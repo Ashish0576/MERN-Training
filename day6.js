46688318aaa398045651529e933e59d921dd4bad
Ashish0576 <ashishrwtt06@gmail.com>
2025-05-14 15:07:30 +0530

    New File

---
+++ day6.js
@@ -0,0 +1,7 @@
+console.log(innerHeight);
+console.log(innerWidth);
+
+function ABC()
+{
+    let x=window.open (self,height=350,width=200);
+}
--- webtec.js
+++
@@ -1,7 +0,0 @@
-var greeter="hey hi";
-var times=4;
-if(times>3){
-    let greet="say hello instead";
-    console.log(greet);
-}
-console.log(greet);
